4b15a6532708f5461299713f1e49609dc2ff8c33
dipc99 <dipalichaudhari99@gmail.com>
2023-01-29 02:36:28 +0530

    Repo1Project1

---
+++ src/main/java/com/BikkadIT/FirstSpringAppUsingide/FirstAppUsingIdeApplication.java
@@ -0,0 +1,15 @@
+package com.BikkadIT.FirstSpringAppUsingide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppUsingIdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppUsingIdeApplication.class, args);
+		
+		System.out.println("First Program Using IDE");
+	}
+
+}
